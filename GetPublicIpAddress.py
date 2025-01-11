d3f3f866c23adf39cec8dbeabd5f8b3adfd49f1b
Paul Ritter <github.proud131@passinbox.com>
2025-01-11 10:53:32 +0100

    initial commit

--- GetPublicIpAddress.py
+++ GetPublicIpAddress.py
@@ -1,32 +1,29 @@
-import os 
+import logging
+import os
 import time
 
+import requests
 from fritzconnection import FritzConnection
 
-userFile=os.getenv('FRITZBOX_USER_FILE', '/run/secrets/user')
-passwordFile=os.getenv('FRITZBOX_PASSWORD_FILE', '/run/secrets/password')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-with open(userFile, 'r', encoding='utf-8') as userFile:
-    user = userFile.read()
-with open(passwordFile, 'r', encoding='utf-8') as passwordFile:
-    password = passwordFile.read()
-
-address=os.getenv('FRITZBOX_ADDRESS', 'fritz.box')
-file=os.getenv('PUBLIC_ADDRESS_FILE', '/out/public_address')
-outputPrefix=os.getenv('OUTPUT_PREFIX', '')
-intervall=int(os.getenv('INTERVALL', '300'))
-
-last_external_ip = None
+lambdaUrl = os.getenv('LAMBDA_FN_URL')
+domain = os.getenv('DOMAIN')
+secret = os.getenv('SECRET')
+queryIntervalInSeconds = int(os.getenv('QUERY_INTERVAL_IN_SECONDS', '300'))
+lastExternalIp = None
+connection = FritzConnection(address=os.getenv('FRITZBOX_ADDRESS', 'fritz.box'))
 
 while True:
-    fc = FritzConnection(address=address, user=user, password=password)
-    external_ip = fc.call_action('WANIPConn1', 'GetExternalIPAddress')['NewExternalIPAddress']
+    externalIp = connection.call_action('WANIPConn1', 'GetExternalIPAddress')['NewExternalIPAddress']
 
-    if external_ip != last_external_ip:
-        last_external_ip = external_ip
-        with open(file, 'w', encoding='utf-8') as output:
-            output.write(f"{outputPrefix}{external_ip}")
+    if externalIp != lastExternalIp:
+        lastExternalIp = externalIp
 
-        print(f"Public IP: {external_ip}")
+        response = requests.get(lambdaUrl, headers={'Content-Type': 'application/json'},
+                                params={'domain': domain, 'secret': secret, 'ip': externalIp})
 
-    time.sleep(intervall)
+        logger.info(response.json())
+
+    time.sleep(queryIntervalInSeconds)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# fritzpublicconnection
-Build docker image with pip package fritzconnection and use it to gather the public IP address of the fritzbox
+# fritz-ddns
+
+Provides Docker image in order to query the public IP of a FritzBox router for DDNS using the Python library
+[fritzconnection](https://github.com/kbr/fritzconnection).
+
+Although the FritzBox provides a DDNS service, it does not seem to work when the box is at the same time used as a
+WireGuard client to a commercial VPN provider (Proton VPN in my case). This applies to both its DDNS setting for
+custom DDNS providers and the resolution of the myfritz.net domain. In the former case, the log shows "DynDNS-Fehler:
+Fehler bei der DNS-Auflösung des Domainnamens.", in the latter case "MyFRITZ! Fehler: Fehler bei der DNS-Auflösung
+des MyFRITZ!-Namens".
+
+The docker image provided here uses the queried IP to call
+an [AWS Lambda function URL](https://docs.aws.amazon.com/lambda/latest/dg/urls-configuration.html), which changes 
+an existing DNS record in Route53.
+
